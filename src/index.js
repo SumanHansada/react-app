d6c8204c5ab2fe2d918937fbe6eaa129ea34fe54
Suman Kumar Hansada <sumansjs@gmail.com>
2019-11-03 19:26:10 +0530

    understanding var, let and const

--- src/index.js
+++ src/index.js
@@ -1,6 +1,31 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+// var -> function scoped
+// let -> block scoped
+// const -> block scoped
+// value declared with const cannot be reassigned
 
-const element = <h1>Hello World</h1>;
-// console.log(element);
-ReactDOM.render(element, document.getElementById('root'));
+function sayHello() {
+    console.log('Say Hello');
+    for (var i = 0; i < 5; i++) {
+        console.log(i);
+    }
+    // But i is still accesible outside the scope
+    console.log(i);
+}
+
+sayHello();
+
+function sayHelloAgain() {
+    console.log('Say Hello Again');
+    for (let i = 0; i < 5; i++) {
+        console.log(i);
+    }
+    // Now i is not accesible outside the scope
+    // console.log(i);
+}
+
+sayHelloAgain();
+
+const x = 1;
+// value declared with const cannot be reassigned.
+// x = 2;
+console.log(x);

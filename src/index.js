15db0f2b01d37160b4ac10c7d4046a187b8b9dbf
Suman Kumar Hansada <sumansjs@gmail.com>
2019-11-05 01:01:18 +0530

    using named and default exports

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
-import {
-    Teacher
+import Teacher, {
+    promote
 } from './teacher';
+
+// Here we are using default as well as named exports
+// Default Exports -> import ... from '';
+// Named Exports -> import { ... } from '';
 
 const teacher = new Teacher('Suman', 'BE');
 console.log(teacher.name);

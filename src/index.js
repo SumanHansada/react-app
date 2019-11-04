572760fd2ed66a6e441ad58020fcb84ac754d631
Suman Kumar Hansada <sumansjs@gmail.com>
2019-11-04 21:14:46 +0530

    using spread operator

--- src/index.js
+++ src/index.js
@@ -1,27 +1,42 @@
-// Object destructuring
+const first = [1, 2, 3];
+const second = [3, 4, 5];
 
-const address = {
-    street: 'Baner',
-    city: 'Pune',
-    country: 'India'
+// Combining traditional way
+const combined = first.concat(second);
+console.log(combined);
+// [1, 2, 3, 3, 4, 5]
+
+// Combining using spread operator
+const combinedAnother = [...first, 'a', ...second, 'b'];
+console.log(combinedAnother);
+// [1, 2, 3, "a", 3, 4, 5, "b"]
+
+const clone = [...first];
+console.log(first);
+//  [1, 2, 3]
+
+console.log(clone);
+//  [1, 2, 3]
+
+// spread operator can be also used to combine objects
+const obj1 = {
+    name: 'Suman'
+};
+const obj2 = {
+    job: 'Software Engineer'
 };
 
-// Traditional Way
-const street1 = address.street;
-const city1 = address.city;
-const country1 = address.country;
+const combinedObject = {
+    ...obj1,
+    ...obj2,
+    location: 'Pune'
+};
+console.log(combinedObject);
+// {name: "Suman", job: "Software Engineer", location: "Pune"}
 
-// Modern Way
-const {
-    street,
-    city,
-    country
-} = address;
-
-// If we need to use alias. Here value of street is stored in newStreet
-const {
-    street: newStreet
-} = address;
-
-console.log(newStreet);
-// Baner
+// spread operator can be also used again to clone the object
+const clonedObj = {
+    ...obj1
+};
+console.log(clonedObj);
+// {name: "Suman"}

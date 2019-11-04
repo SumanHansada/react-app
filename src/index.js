ddb981fe1395b3cc8850f5a73df5aea4bdab6972
Suman Kumar Hansada <sumansjs@gmail.com>
2019-11-05 00:17:11 +0530

    using inhertance

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+// Using class
 class Person {
     constructor(name) {
         this.name = name;
@@ -7,6 +8,41 @@ class Person {
     }
 }
 
-const person = new Person('Suman');
-console.log(person.name);
+// Using inheritance
+// Here Teacher class inherits all the properties of Person class
+class Teacher extends Person {
+    teach() {
+        console.log('teach');
+    }
+}
+
+const teacher = new Teacher('Suman');
+// so now teacher object can access person constructor
+console.log(teacher.name);
 // Suman
+console.log(teacher.walk());
+// walk
+console.log(teacher.teach());
+// teach
+
+// Taking it to next level
+// Passing the name to parent class
+class NewTeacher extends Person {
+    constructor(name, degree) {
+        super(name);
+        this.degree = degree;
+    }
+    teach() {
+        console.log('teach');
+    }
+}
+
+const newTeacher = new NewTeacher('Suman', 'BE');
+console.log(newTeacher.name);
+// Suman
+console.log(newTeacher.degree);
+// BE
+console.log(newTeacher.walk());
+// walk
+console.log(newTeacher.teach());
+// teach

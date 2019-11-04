6d8eb6edbf6da3bed8dc6645fc43e9cbb7a08033
Suman Kumar Hansada <sumansjs@gmail.com>
2019-11-05 00:04:50 +0530

    using class

--- src/index.js
+++ src/index.js
@@ -1,42 +1,12 @@
-const first = [1, 2, 3];
-const second = [3, 4, 5];
+class Person {
+    constructor(name) {
+        this.name = name;
+    }
+    walk() {
+        console.log('walk');
+    }
+}
 
-// Combining traditional way
-const combined = first.concat(second);
-console.log(combined);
-// [1, 2, 3, 3, 4, 5]
-
-// Combining using spread operator
-const combinedAnother = [...first, 'a', ...second, 'b'];
-console.log(combinedAnother);
-// [1, 2, 3, "a", 3, 4, 5, "b"]
-
-const clone = [...first];
-console.log(first);
-//  [1, 2, 3]
-
-console.log(clone);
-//  [1, 2, 3]
-
-// spread operator can be also used to combine objects
-const obj1 = {
-    name: 'Suman'
-};
-const obj2 = {
-    job: 'Software Engineer'
-};
-
-const combinedObject = {
-    ...obj1,
-    ...obj2,
-    location: 'Pune'
-};
-console.log(combinedObject);
-// {name: "Suman", job: "Software Engineer", location: "Pune"}
-
-// spread operator can be also used again to clone the object
-const clonedObj = {
-    ...obj1
-};
-console.log(clonedObj);
-// {name: "Suman"}
+const person = new Person('Suman');
+console.log(person.name);
+// Suman

3c02c9e2e7edf138772ab644e8b79fa73737c931
Suman Kumar Hansada <sumansjs@gmail.com>
2019-11-04 20:11:28 +0530

    using object destructuring

--- src/index.js
+++ src/index.js
@@ -1,9 +1,27 @@
-// map returns the new array
-const colors = ['Red', 'Green', 'Blue'];
-const items = colors.map(color => `<li>${color}</li>`);
+// Object destructuring
 
-console.log(items);
-// ["<li>Red</li>", "<li>Green</li>", "<li>Blue</li>"]
-// 0: "<li>Red</li>"
-// 1: "<li>Green</li>"
-// 2: "<li>Blue</li>"
+const address = {
+    street: 'Baner',
+    city: 'Pune',
+    country: 'India'
+};
+
+// Traditional Way
+const street1 = address.street;
+const city1 = address.city;
+const country1 = address.country;
+
+// Modern Way
+const {
+    street,
+    city,
+    country
+} = address;
+
+// If we need to use alias. Here value of street is stored in newStreet
+const {
+    street: newStreet
+} = address;
+
+console.log(newStreet);
+// Baner

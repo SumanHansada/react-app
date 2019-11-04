039c94ef58d8e5ed9f77bed00e2bd266e40b975b
Suman Kumar Hansada <sumansjs@gmail.com>
2019-11-04 20:00:28 +0530

    using Array.map method

--- src/index.js
+++ src/index.js
@@ -1,54 +1,9 @@
-const person = {
-    talk() {
-        console.log('this', this);
-    }
-}
+// map returns the new array
+const colors = ['Red', 'Green', 'Blue'];
+const items = colors.map(color => `<li>${color}</li>`);
 
-person.talk();
-// Here this point to person object
-// {talk: ƒ}
-// talk: ƒ talk()
-
-const newPerson = {
-    talk() {
-        setTimeout(function () {
-            console.log('this', this);
-        }, 1000)
-    }
-}
-
-newPerson.talk();
-// Here this points to window(global) object
-// Window {parent: Window, postMessage: ƒ, blur: ƒ, focus: ƒ, close: ƒ, …}
-
-// Reason -
-// Here callback function - function() {console.log('this', this)} is not the part of object
-// It is a standalone function
-// So callback function returns the reference of the window object
-
-// Solution -
-const anotherPerson = {
-    talk() {
-        var self = this;
-        setTimeout(function () {
-            console.log('self', self);
-        }, 1000);
-    }
-}
-
-anotherPerson.talk();
-// {talk: ƒ}
-// talk: ƒ talk()
-
-// Another way - Using Arrow Functions
-// Here this inherits the context in which it is defined
-// Arrow functions don't rebind the this keyword
-const person1 = {
-    talk() {
-        setTimeout(() => console.log('this', this), 1000)
-    }
-}
-
-person1.talk();
-// {talk: ƒ}
-// talk: ƒ talk()
+console.log(items);
+// ["<li>Red</li>", "<li>Green</li>", "<li>Blue</li>"]
+// 0: "<li>Red</li>"
+// 1: "<li>Green</li>"
+// 2: "<li>Blue</li>"

ce0e05470b56538eea062a65aa0f8845e894f840
Suman Kumar Hansada <sumansjs@gmail.com>
2019-11-05 15:15:23 +0530

    rendering lists

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -5,18 +5,20 @@ class Counter extends Component {
   // state objects includes any data that a component needs
   state = {
     count: 0,
-    imageUrl: 'https://picsum.photos/seed/picsum/450/300'
+    imageUrl: 'https://picsum.photos/seed/picsum/450/300',
+    tags: ['tag1', 'tag2', 'tag3']
   };
 
   // we can set the styles by declaring it as an object
   styles = {
-    fontSize: 50,
+    fontSize: 20,
     fontWeight: 'bold'
   };
 
   render() {
     // We can write any valid javascript expression inside {}
     // since class is a reserved keyword in javascript, we use className
+    // We can use map operator to render the list
     return (
       <React.Fragment>
         <img src={this.state.imageUrl} alt='' />
@@ -24,6 +26,11 @@ class Counter extends Component {
           {this.formatCount()}
         </span>
         <button className='btn btn-secondary btn-sm'>Increment</button>
+        <ul>
+          {this.state.tags.map(tag => (
+            <li key={tag}>{tag}</li>
+          ))}
+        </ul>
       </React.Fragment>
     );
   }

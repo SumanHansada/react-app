67f643242f0a82fb402bcf44767f196bf502fc5c
Suman Kumar Hansada <sumansjs@gmail.com>
2019-11-05 17:46:59 +0530

    conditional rendering

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -5,7 +5,6 @@ class Counter extends Component {
   // state objects includes any data that a component needs
   state = {
     count: 0,
-    imageUrl: 'https://picsum.photos/seed/picsum/450/300',
     tags: ['tag1', 'tag2', 'tag3']
   };
 
@@ -16,30 +15,24 @@ class Counter extends Component {
   };
 
   render() {
-    // We can write any valid javascript expression inside {}
-    // since class is a reserved keyword in javascript, we use className
-    // We can use map operator to render the list
     return (
       <React.Fragment>
-        <img src={this.state.imageUrl} alt='' />
-        <span style={this.styles} className={this.getBadgeClasses()}>
-          {this.formatCount()}
-        </span>
-        <button className='btn btn-secondary btn-sm'>Increment</button>
-        <ul>
-          {this.state.tags.map(tag => (
-            <li key={tag}>{tag}</li>
-          ))}
-        </ul>
+        {this.state.tags.length === 0 && 'Please create a new tag'}
+        {this.renderTags()}
       </React.Fragment>
     );
   }
 
-  // we can set classname dynamically
-  getBadgeClasses() {
-    let classes = 'badge m-2 badge-';
-    classes += this.state.count === 0 ? 'warning' : 'primary';
-    return classes;
+  // To conditionally render the tags, we can seperate the logic in seperate method
+  renderTags() {
+    if (this.state.tags.length === 0) return <p>There are no tags!</p>;
+    return (
+      <ul>
+        {this.state.tags.map(tag => (
+          <li key={tag}>{tag}</li>
+        ))}
+      </ul>
+    );
   }
 
   formatCount() {

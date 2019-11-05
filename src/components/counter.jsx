2b135896e74c194daf47a03a4d41a3f6c0c535e4
Suman Kumar Hansada <sumansjs@gmail.com>
2019-11-05 10:37:58 +0530

    setting the attributes

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -4,14 +4,26 @@ import React, { Component } from 'react';
 class Counter extends Component {
   // state objects includes any data that a component needs
   state = {
-    count: 0
+    count: 0,
+    imageUrl: 'https://picsum.photos/seed/picsum/450/300'
   };
+
+  // we can set the styles by declaring it as an object
+  styles = {
+    fontSize: 50,
+    fontWeight: 'bold'
+  };
+
   render() {
-    // We can write any valid javasript expression inside {}
+    // We can write any valid javascript expression inside {}
+    // since class is a reserved keyword in javascript, we use className
     return (
       <React.Fragment>
-        <span>{this.formatCount()}</span>
-        <button>Increment</button>
+        <img src={this.state.imageUrl} alt='' />
+        <span style={this.styles} className='badge badge-primary m-2'>
+          {this.formatCount()}
+        </span>
+        <button className='btn btn-secondary btn-sm'>Increment</button>
       </React.Fragment>
     );
   }

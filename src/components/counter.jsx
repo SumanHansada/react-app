ac021b315894117ee3e47daa6d2ceea88443b0b3
Suman Kumar Hansada <sumansjs@gmail.com>
2019-11-05 08:56:09 +0530

    using React.Fragment

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,9 +1,15 @@
 import React, { Component } from 'react';
 
+// For using multiple html elements without div, we use React.Fragment
 class Counter extends Component {
-    render() { 
-        return <h1>Hello World</h1>;
-    }
+  render() {
+    return (
+      <React.Fragment>
+        <h1>Hello World</h1>
+        <button>Increment</button>
+      </React.Fragment>
+    );
+  }
 }
- 
+
 export default Counter;

706ffeefaac36dfc420c62bc4a441e4609b5a04b
Suman Kumar Hansada <sumansjs@gmail.com>
2019-11-05 09:14:09 +0530

    embedding expressions

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,13 +2,22 @@ import React, { Component } from 'react';
 
 // For using multiple html elements without div, we use React.Fragment
 class Counter extends Component {
+  // state objects includes any data that a component needs
+  state = {
+    count: 0
+  };
   render() {
+    // We can write any valid javasript expression inside {}
     return (
       <React.Fragment>
-        <h1>Hello World</h1>
+        <span>{this.formatCount()}</span>
         <button>Increment</button>
       </React.Fragment>
     );
+  }
+  formatCount() {
+    const { count } = this.state;
+    return count === 0 ? 'Zero' : count;
   }
 }
 

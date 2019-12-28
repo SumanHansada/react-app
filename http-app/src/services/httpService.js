7c48b9018a22cca2cb93daa1bf9850376dd832dc
Suman Kumar Hansada <sumansjs@gmail.com>
2019-12-28 16:38:22 +0530

    backend services - displaying toast notification

--- http-app/src/services/httpService.js
+++ http-app/src/services/httpService.js
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { toast } from "react-toastify";
 
 axios.interceptors.response.use(null, error => {
   const expectedError =
@@ -11,7 +12,7 @@ axios.interceptors.response.use(null, error => {
     // - Log them
     // - Display a generic and friendly error message
     console.log("Logging the error", error);
-    alert("An unexpected error occured");
+    toast.error("An unexpected error occured");
   }
   return Promise.reject(error);
 });

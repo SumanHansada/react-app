c20188770ed056bb4fe0956971111d5d90543e9b
Suman Kumar Hansada <sumansjs@gmail.com>
2019-12-28 19:18:10 +0530

    backend services - logging errors

--- http-app/src/services/httpService.js
+++ http-app/src/services/httpService.js
@@ -1,5 +1,12 @@
 import axios from "axios";
 import { toast } from "react-toastify";
+import * as Sentry from "@sentry/browser";
+
+Sentry.init({
+  dsn: "https://f5064590402c490c97ec46791baa8255@sentry.io/1867784",
+  environment: "development-test",
+  release: "1-0-0"
+});
 
 axios.interceptors.response.use(null, error => {
   const expectedError =
@@ -11,7 +18,7 @@ axios.interceptors.response.use(null, error => {
     // Unexpected Errors (network down, server down, db down, bug)
     // - Log them
     // - Display a generic and friendly error message
-    console.log("Logging the error", error);
+    Sentry.captureException(error);
     toast.error("An unexpected error occured");
   }
   return Promise.reject(error);

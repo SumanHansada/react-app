5c07ac0c15ac2a584e9b4e9f06610568602ed5b6
Suman Kumar Hansada <sumansjs@gmail.com>
2020-01-01 07:25:30 +0530

    vidly - showing or hiding elements based on user

--- vidly/src/components/movies.jsx
+++ vidly/src/components/movies.jsx
@@ -104,6 +104,8 @@ class Movies extends Component {
       genres
     } = this.state;
 
+    const { user } = this.props;
+
     if (moviesCount === 0) return <p>There are no movies in the database</p>;
 
     const { totalCount, data: movies } = this.getPagedData();
@@ -118,13 +120,15 @@ class Movies extends Component {
           />
         </div>
         <div className="col">
-          <Link
-            to="movies/new"
-            className="btn btn-primary"
-            style={{ marginBotton: 20 }}
-          >
-            New Movie
-          </Link>
+          {user && (
+            <Link
+              to="movies/new"
+              className="btn btn-primary"
+              style={{ marginBotton: 20 }}
+            >
+              New Movie
+            </Link>
+          )}
           <p>Showing {totalCount} movies in the database.</p>
           <SearchBox value={searchQuery} onChange={this.handleSearch} />
           <MoviesTable
